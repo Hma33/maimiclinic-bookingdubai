f0d404d4e02dc33683bb1a9669a73c5c6bd2814b
Hma33 <141799218+Hma33@users.noreply.github.com>
2026-01-20 02:27:25 +0700

    Update booking_app.py

--- booking_app.py
+++ booking_app.py
@@ -300,7 +300,6 @@ with col1:
 
 # === RIGHT COLUMN: INFO PANEL ===
 with col2:
-    # 🦷 Tooth icon removed here
     st.markdown("## Miami Dental Clinic")
     st.markdown("---")
     st.markdown("""
@@ -320,5 +319,3 @@ with col2:
     """)
     st.markdown("---")
     st.markdown("📍 **[View on Google Map](https://www.google.com/maps/place/Miami+General+Dental+Clinic/@25.2636757,55.325599,17z/data=!3m1!4b1!4m6!3m5!1s0x3e5f5d1e0d3058ad:0x3a8d8b12b7f74d3b!8m2!3d25.2636757!4d55.325599!16s%2Fg%2F11wjbrtggl?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoASAFQAw%3D%3D)**")
-    st.write("")
-    st.info("ℹ️ **System Status:** Online | Data is saving to Google Sheets.")
